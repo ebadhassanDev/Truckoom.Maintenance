58d96c53cd69b1d756e23199750d97680e97d0d6
eHassan <ebadhassan.dev@gmail.com>
2024-12-26 12:40:58 +0500

    initial setup

--- src/Truckoom.Maintenance.Api/Program.cs
+++ src/Truckoom.Maintenance.Api/Program.cs
@@ -1,9 +1,46 @@
-var builder = WebApplication.CreateBuilder(args);
 
+var builder = WebApplication.CreateBuilder(args);
+var jwtPolicyName = "jwt";
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen(setup =>
+{
+    setup.SwaggerDoc("v1", new OpenApiInfo()
+    {
+        Description = "ASP.NET Core 8.0 - Minimal API Example - Todo API implementation using ASP.NET Core Minimal API," +
+            "Entity Framework Core, Token authentication, Versioning, Unit Testing and Open API.",
+        Title = "Todo Api",
+        Version = "v1",
+        Contact = new OpenApiContact()
+        {
+            Name = "anuraj",
+            Url = new Uri("https://dotnetthoughts.net")
+        }
+    });
 
+    setup.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.Http,
+        Scheme = "Bearer"
+    });
+
+    setup.OperationFilter<AuthorizationHeaderOperationHeader>();
+    setup.OperationFilter<ApiVersionOperationFilter>();
+});
+builder.Services.AddEndpointsApiExplorer();
+builder.WebHost.UseKestrel(options => options.AddServerHeader = false);
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddApiVersioning(options =>
+{
+    options.DefaultApiVersion = new ApiVersion(1, 0);
+    options.ReportApiVersions = true;
+    options.AssumeDefaultVersionWhenUnspecified = true;
+    options.ApiVersionReader = new HeaderApiVersionReader("api-version");
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -13,6 +50,11 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Truckoom Maintenance v1");
+});
 
 var summaries = new[]
 {
@@ -21,7 +63,7 @@ var summaries = new[]
 
 app.MapGet("/weatherforecast", () =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
+    var forecast = Enumerable.Range(1, 5).Select(index =>
         new WeatherForecast
         (
             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
@@ -32,6 +74,18 @@ app.MapGet("/weatherforecast", () =>
     return forecast;
 })
 .WithName("GetWeatherForecast");
+
+// app.MapGet("/health", async (HealthCheckService healthCheckService) =>
+// {
+//     var report = await healthCheckService.CheckHealthAsync();
+//     return report.Status == HealthStatus.Healthy ?
+//         Results.Ok(report) : Results.StatusCode(StatusCodes.Status503ServiceUnavailable);
+// }).WithOpenApi()
+// .WithTags(["Health"])
+// .RequireRateLimiting(jwtPolicyName)
+// .Produces(200)
+// .ProducesProblem(503)
+// .Produces(429);
 
 app.Run();
 
