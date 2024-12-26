7ed2b1bf9ea6eb3c2f207c7ebc6a942cd6c1e05a
eHassan <ebadhassan.dev@gmail.com>
2024-12-26 12:35:29 +0500

    auth and versioning updated

---
+++ src/Truckoom.Maintenance.Api/Usings.cs
@@ -0,0 +1,11 @@
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.Extensions.Diagnostics.HealthChecks;
+global using Microsoft.OpenApi.Models;
+global using Truckoom.Maintenance.Api;
+global using System.IdentityModel.Tokens.Jwt;
+global using System.Security.Claims;
+global using Asp.Versioning;
+
+global using System.Diagnostics;
+global using System.Net.Http.Headers;
+global using System.Threading.RateLimiting;
